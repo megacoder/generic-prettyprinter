5a61f09e8b35cb04f4db268053f0f1547bc1563e
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-07-07 17:27:12 -0500

    Blkid printer.

---
+++ gpp/plugins/blkid-plugin.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# vim: noet sw=4 ts=4
+
+import	os
+import	sys
+import	string
+import	superclass
+
+class	PrettyPrint( superclass.MetaPrettyPrinter ):
+
+	NAME = 'blkid-pp'
+	DESCRIPTION="""List output of blkid(8) in a canonical form."""
+
+	INDENT_WITH = '        '
+
+	def __init__( self ):
+		super( PrettyPrint, self ).__init__()
+		return
+
+	def	reset( self ):
+		self.lines = []
+		self.max_name = 15
+		return
+
+	def	process( self, f = sys.stdin ):
+		for line in f:
+			tokens = line.rstrip().split( ':', 1 )
+			if len(tokens) != 2: continue
+			self.max_name = max( self.max_name, len( tokens[0] ) )
+			self.lines.append( (tokens[0], tokens[1].split()) )
+		return
+
+	def	finish( self ):
+		self.lines.sort( key = lambda (n,a): string.lower(n) )
+		fmt = '%%%ds : %%s' % self.max_name
+		for (name, args) in self.lines:
+			args.sort()
+			print fmt % ( name, ' '.join(args) )
+		return
