6e7d250a831e4ce85eecab19988e514e5dd30fac
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-07-07 17:23:27 -0500

    Relocated.

--- gpp/plugins/lvm-plugin.py
+++ gpp/plugins/lvm-plugin.py
@@ -4,21 +4,33 @@
 import	os
 import	sys
 import	string
+import	superclass
 
-class	LvmPrettyPrint( object ):
+class	PrettyPrint( superclass.MetaPrettyPrinter ):
+
+	NAME = 'lvm-pp'
+	DESCRIPTION = """Display LVM configuration files in a canonical form."""
 
 	INDENT_WITH = '        '
 
 	def __init__( self ):
-		self._reset()
+		super( PrettyPrint, self ).__init__()
 		return
 
-	def	_reset( self ):
+	def	reset( self ):
 		self.stanza = []
 		self.max_name = 15
 		return
 
-	def	_process( self, f = sys.stdin ):
+	def	dump_stanza( self ):
+		fmt = '%%%ds = %%s' % self.max_name
+		self.stanza.sort()
+		for tokens in self.stanza:
+			print fmt % ( tokens[0], ' '.join( tokens[1:] ) )
+		self.reset()
+		return
+
+	def	process( self, f = sys.stdin ):
 		for line in f:
 			# Column-1 comments are copied verbatim
 			if line.lstrip().startswith( '#' ):
@@ -29,18 +41,13 @@ class	LvmPrettyPrint( object ):
 				l = line.rstrip().replace( '{', ' { ' ).replace( '}', ' } ' )
 				tokens = l.split()
 				print '\t'.join( tokens )
-				self._reset()
+				# Dump any cruft from a partially-completed stanza
+				self.reset()
 			elif line.find( '}' ) > -1:
-				# Stanza ending
-				fmt = '%%%ds = %%s' % self.max_name
-				self.stanza.sort()
-				for tokens in self.stanza:
-					print fmt % ( tokens[0], ' '.join( tokens[1:] ) )
-				#
+				self.dump_stanza()
 				l = line.rstrip().replace( '{', ' { ' ).replace( '}', ' } ' )
 				tokens = l.split()
 				print '\t'.join( tokens )
-				self._reset()
 			else:
 				tokens = line.rstrip().split( '=' )
 				if len(tokens) != 2:
@@ -49,46 +56,3 @@ class	LvmPrettyPrint( object ):
 				self.max_name = max( self.max_name, len(tokens[0]) )
 				self.stanza.append( tokens )
 		return
-
-	def do_file( self, fn ):
-		if not os.path.isfile( fn ): return
-		f = open( fn, 'rt' )
-		self._process( f )
-		f.close()
-		return
-
-	def	do_dir( self, dn ):
-		try:
-			files = os.listdir( dn )
-		except Exception, e:
-			print >>sys.stderr, 'Cannot list directory "%s".' % dn
-			raise e
-		files.sort()
-		for file in files:
-			self.do_name( os.path.join( dn, file ) )
-		return
-
-	def	do_name( self, name ):
-		if os.path.isfile( name ):
-			self.do_file( name )
-		elif os.path.isdir( name ):
-			self.do_dir( name )
-		else:
-			# print >>sys.stderr, 'Ignoring "%s".' % name
-			pass
-		return
-
-if __name__ == '__main__':
-	lpp = LvmPrettyPrint()
-	me = os.path.basename( sys.argv[0] )
-	if len(sys.argv) == 1:
-		lpp._process()
-		# lpp.do_file( '/etc/multipath.conf' )
-		# lpp.do_directory( '/etc/multipath.d' )
-	else:
-		for arg in sys.argv[1:]:
-			if arg == '-':
-				lpp._process()
-			else:
-				lpp.do_name( arg )
-	exit(0)
