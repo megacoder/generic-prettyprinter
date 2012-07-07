8a455a7d6c1a8e62bd270d2e878f444d3e62707c
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-07-07 11:23:30 -0500

    Display OVM "vm.cfg" files in canonical style.

--- src/plugins/vmcfg-plugin.py
+++ src/plugins/vmcfg-plugin.py
@@ -2,11 +2,15 @@
 
 import  pprint
 import  sys
+import  superclass
 
-class   PrettyPrint( object ):
+class   PrettyPrint( superclass.MetaPrettyPrinter ):
+
+    NAME = 'vmcfg'
+    DESCRIPTION = """Display vm.cfg files in canonical style."""
 
     def __init__( self ):
-        self.reset()
+        super( PrettyPrint, self ).__init__()
         return
 
     def reset( self ):
@@ -14,13 +18,13 @@ class   PrettyPrint( object ):
         self.code   = None
         self.fn     = None
         self.locals = None
-        self.pp     = pprint.PrettyPrint()
         return
 
     def compile( self ):
         try:
             self.code = compile( self.script, self.fn, 'exec' )
         except:
+            self.error( 'File "%s" appears to be corrupt.' % self.fn )
             raise SyntaxError
         self.locals = dict()
         globals = dict()
@@ -32,13 +36,28 @@ class   PrettyPrint( object ):
         self.keys.sort()
         return
 
+    def do_file( self, fn ):
+        self.fn = fn
+        self.compile()
+        return
+
+    def process( self, f = None ):
+        if f is None:
+            self.fn = '{stdin}'
+            self.script = sys.stdin.readlines()
+            self.compile()
+        return
+
     def finish( self ):
-        for key in self.keys:
-            print '%13s = ' % key,
-            s = self.pp.pformat( self.locals[key] )
-            lines = s.split( '\n' )
-            leadin = ''
-            for line in lines:
-                print '%s%s' % (leadin, line)
-                leadin = ' '*(16+2)
+        if self.keys is None:
+            self.error( 'No input detected.' )
+        else:
+            for key in self.keys:
+                print '%13s = ' % key,
+                s = pprint.PrettyPrint.pformat( self.locals[key] )
+                lines = s.split( '\n' )
+                leadin = ''
+                for line in lines:
+                    print '%s%s' % (leadin, line)
+                    leadin = ' '*(16+2)
         return
