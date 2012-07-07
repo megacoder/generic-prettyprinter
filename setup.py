709b8e2711ff182aa86333ec34a2b14cd4144f01
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-07-07 11:37:59 -0500

    Primal setup script;

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# vim: noet sw=4 ts=4
+
+from distutils.core import setup
+
+setup(
+	name = 'gpp',
+	version = '@VERSION@',
+	description = 'Generic file pretty printer, using plugins.',
+	author = 'Tommy Reynolds',
+	author_email = 'Tommy.Reynolds@MegaCoder.com',
+	url = 'http://www.MegaCoder.com',
+	py_modules=[ 'gpp' ]
+	package_dir=[ '': 'src' ]
+)
