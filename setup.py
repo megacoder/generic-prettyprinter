90b09902423c27d95bee2b46599ec3332874db9b
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-07-08 19:51:05 -0500

    Checkpoint

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name         ='gpp',
+	version      ='1.0',
+	description  ='Generic Pretty Printer, using plugins',
+	author       ='Tommy Reynolds',
+	author_email ='Tommy.Reynolds@MegaCoder.com',
+	url          ='http://www.MegaCoder.com',
+	packages     =['src', 'src/plugins'],
+)
