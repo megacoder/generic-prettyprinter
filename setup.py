79285b47be369f0cc521c02fb67ac663778cb35d
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-07-12 13:53:41 -0500

    Checkpoint.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from	distutils.core			import	setup
+import	glob
+import	os
+from	DistUtilsExtra.command	import	*
 
 setup(
 	name         ='gpp',
@@ -9,7 +12,13 @@ setup(
 	author       ='Tommy Reynolds',
 	author_email ='Tommy.Reynolds@MegaCoder.com',
 	url          ='http://www.MegaCoder.com',
+	long_description=open('README').read(),
 	packages     =['gpp' ],
 	package_dir = { 'gpp':'src/gpp' },
 	package_data = { 'gpp': [ 'plugins/*.py' ] },
+	entry_points= {
+		'console_scripts': [
+			'gpp = gpp:Main'
+		]
+	},
 )
