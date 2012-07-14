39b66f212bd8a9558ae92c21d50d12a016d35d8d
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-07-14 17:16:53 -0500

    Lo and behold, Houston, we have a script in the infrastructure.

--- setup.py
+++ setup.py
@@ -6,19 +6,23 @@ import	os
 from	DistUtilsExtra.command	import	*
 
 setup(
-	name         ='gpp',
-	version      ='1.0',
-	description  ='Generic Pretty Printer, using plugins',
-	author       ='Tommy Reynolds',
-	author_email ='Tommy.Reynolds@MegaCoder.com',
-	url          ='http://www.MegaCoder.com',
-	long_description=open('README').read(),
-	packages     =['gpp' ],
-	package_dir = { 'gpp':'src/gpp' },
-	package_data = { 'gpp': [ 'plugins/*.py' ] },
-	entry_points= {
+	name             ='gpp',
+	version          ='1.0',
+	description      ='Generic Pretty Printer, using plugins',
+	author           ='Tommy Reynolds',
+	author_email     ='Tommy.Reynolds@MegaCoder.com',
+	license          ='GPL',
+	url              ='http://www.MegaCoder.com',
+	long_description =open('README').read(),
+	packages         =['gpp' ],
+	package_dir      ={ 'gpp':'src/gpp' },
+	package_data     ={ 'gpp': [ 'plugins/*.py' ] },
+	entry_points     ={
 		'console_scripts': [
 			'gpp = gpp:Main'
 		]
 	},
+	scripts			=[
+		'scripts/gpp.sh'
+	],
 )
--- src/gpp/scripts/gpp.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-#vim: noet sw=4 ts=4
-import	os
-import	sys
-
-print sys.argv
