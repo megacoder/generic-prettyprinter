0e26518eacc3f5d3df08749fdb0f353b6ad8f74c
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-07-15 00:59:01 -0500

    Changed into plugin form.

--- gpp/plugins/printcap-plugin.py
+++ gpp/plugins/printcap-plugin.py
@@ -2,46 +2,47 @@
 
 import	os
 import	sys
+import	superclass
 
-class	PrintCapPrettyprint( object ):
+class	PrettyPrint( MetaPrettyPrinter ):
 
-		def	__init__( self ):
-			self.reset()
-			return
+	NAME = 'printcap-pp'
+	DESCRIPTION="""Display RHEL-4 printcap files in a readable style."""
 
-		def	reset( self ):
-			return
+	def	__init__( self ):
+		super( PrettyPrint, self ).__init__()
+		return
 
-		def	process( self, f = sys.stdin ):
-			for original in f:
-				line = original.replace( '#.*', '' ).rstrip()
-				try:
-					tokens = line.split( ':' )
-				except Exception, e:
-					print original,
-					continue
-				first = tokens[0]
-				try:
-					name, comment = first.split( '|', 1 )
-				except Exception, e:
-					name = first
-					comment = 'N/A'
-				print '%s "%s"' % ( name, comment )
-				attrs = tokens[1:]
-				attrs.sort()
-				for attr in attrs:
-					if len(attr) > 0:
-						try:
-							name, value = attr.split( '=', 1 )
-							print '%7s = %s' % (name, value)
-						except Exception, e:
-							print '%7s' % attr
-			return
+	def	process( self, f = sys.stdin ):
+		for original in f:
+			line = original.replace( '#.*', '' ).rstrip()
+			try:
+				tokens = line.split( ':' )
+			except Exception, e:
+				print original,
+				continue
+			first = tokens[0]
+			try:
+				name, comment = first.split( '|', 1 )
+			except Exception, e:
+				name = first
+				comment = 'N/A'
+			print '%s "%s"' % ( name, comment )
+			attrs = tokens[1:]
+			attrs.sort()
+			for attr in attrs:
+				if len(attr) > 0:
+					try:
+						name, value = attr.split( '=', 1 )
+						print '%7s = %s' % (name, value)
+					except Exception, e:
+						print '%7s' % attr
+		return
 
-		def	finish( self ):
-			return
+	def	finish( self ):
+		return 0
 
 if __name__ == '__main__':
-	pcp = PrintCapPrettyprint()
+	pcp = PrettyPrint()
 	pcp.process()
 	pcp.finish()
