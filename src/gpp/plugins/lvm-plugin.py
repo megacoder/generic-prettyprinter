4eadf6400c06fd0131f1d92ccc46f199e1be5eed
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-08-16 13:35:38 -0500

    Sort the LVM config stanzas as well as sort their content.

--- src/gpp/plugins/lvm-plugin.py
+++ src/gpp/plugins/lvm-plugin.py
@@ -8,10 +8,8 @@ import	superclass
 
 class	PrettyPrint( superclass.MetaPrettyPrinter ):
 
-	NAME = 'lvm-pp'
+	NAME = 'lvm'
 	DESCRIPTION = """Display LVM configuration files in a canonical form."""
-
-	INDENT_WITH = '        '
 
 	def __init__( self ):
 		super( PrettyPrint, self ).__init__()
@@ -19,16 +17,24 @@ class	PrettyPrint( superclass.MetaPrettyPrinter ):
 
 	def	reset( self ):
 		super( PrettyPrint, self ).reset()
-		self.stanza = []
-		self.max_name = 15
+		self.begins_with = None
+		self.ends_with   = None
+		self.max_name    = 15
+		self.stanza      = []
+		self.stanzas     = []
 		return
 
-	def	dump_stanza( self ):
+	def	dump_stanza( self, beginning, content, ending ):
 		fmt = '%%%ds = %%s' % self.max_name
-		self.stanza.sort()
-		for tokens in self.stanza:
+		content.sort()
+		print '\t'.join(beginning)
+		for tokens in content:
 			print fmt % ( tokens[0], ' '.join( tokens[1:] ) )
-		self.reset()
+		print '\t'.join(ending)
+		return
+
+	def	finish_stanza( self, beginning, content, ending ):
+		self.stanzas.append( (beginning, content, ending) )
 		return
 
 	def	next_line( self, line ):
@@ -38,25 +44,22 @@ class	PrettyPrint( superclass.MetaPrettyPrinter ):
 			return
 		# All other lines are to be correctly indented
 		if line.find( '{' ) > -1:
-			l = line.rstrip().replace( '{', ' { ' ).replace( '}', ' } ' )
-			tokens = l.split()
-			print '\t'.join( tokens )
-			# Dump any cruft from a partially-completed stanza
-			self.reset()
+			self.begins_with = line.rstrip().replace( '{', ' { ' ).replace( '}', ' } ' ).split()
+			self.stanza = []
 		elif line.find( '}' ) > -1:
-			self.dump_stanza()
-			l = line.rstrip().replace( '{', ' { ' ).replace( '}', ' } ' )
-			tokens = l.split()
-			print '\t'.join( tokens )
+			self.ends_with = line.rstrip().replace( '{', ' { ' ).replace( '}', ' } ' ).split()
+			self.finish_stanza( self.begins_with, self.stanza, self.ends_with )
 		else:
-			tokens = line.rstrip().split( '=' )
-			if len(tokens) != 2:
-				print line,
-				return
+			tokens = line.rstrip().split( '=', 1 )
+			if len(tokens) < 2:
+				tokens[2] = '{missing}'
 			self.max_name = max( self.max_name, len(tokens[0]) )
 			self.stanza.append( tokens )
 		return
 
 	def	finish( self ):
-		self.dump_stanza()
+		if len(self.stanzas) > 0:
+			self.stanzas.sort()
+			for (beginning,content,ending) in self.stanzas:
+				self.dump_stanza( beginning, content, ending )
 		return
