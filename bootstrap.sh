60604fed439d39f8568a507a72585c160a105ced
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-09-12 16:07:45 -0500

    Checkpoint

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ if [ $# -eq 0 ]; then
 	# set -- bdist_rpm --spec-only
 	set -- bdist_rpm
 fi
-if [ ! -f README ]; then
-	markdown2 README.md | lynx -dump -stdin >README
+if [ ! -f src/gpp/README.txt ]; then
+	markdown2 README.md | tee src/gpp/README.html | lynx -dump -stdin >src/gpp/README.txt
 fi
 python ./setup.py "${@}"
