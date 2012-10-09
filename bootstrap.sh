7340fb0af6f163bbb009aa731a101f434b83ed2b
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-10-09 15:04:14 -0500

    Let's try to do the RPM bit.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 if [ $# -eq 0 ]; then
-	# set -- bdist --format=bztar
+	set -- bdist --format=bztar
 	# set -- bdist --format=gztar
-	set -- bdist --format=rpm
+	# set -- bdist --format=rpm
 fi
-markdown2 README.md | tee src/gpp/README.html | lynx -dump -stdin >src/gpp/README.txt
+markdown2 README.md | tee README.html | lynx -dump -stdin >README.txt
 python ./setup.py "${@}"
