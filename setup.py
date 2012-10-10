a1d1090b9b6d5ea18d5a6a397c2adc5c2bf77397
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-10-10 15:30:45 -0500

    Format YUM repos.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-VERSION	= '0.0.34'
+VERSION	= '0.0.35'
 
 from	distutils.core			import	setup
 import	glob
--- src/gpp/plugins/yum_repo-plugin.py
+++ src/gpp/plugins/yum_repo-plugin.py
@@ -6,8 +6,8 @@ import	superclass
 
 class	PrettyPrint( superclass.MetaPrettyPrinter ):
 
-	NAME = 'kdump'
-	DESCRIPTION = """Display /etc/kdump.* files in canonical style."""
+	NAME = 'yum.repo'
+	DESCRIPTION = """Display /etc/yum.repos.d/*.repo files in style."""
 
 	def	__init__( self ):
 		super( PrettyPrint, self ).__init__()
@@ -15,7 +15,13 @@ class	PrettyPrint( superclass.MetaPrettyPrinter ):
 
 	def	reset( self ):
 		super( PrettyPrint, self ).reset()
-		self.tokens = []
+		self.feeds    = []
+		self._new_repo()
+		return
+
+	def	_new_repo( self, name = None ):
+		self.feed     = []
+		self.name	  = name
 		self.max_name = 1
 		return
 
@@ -23,20 +29,25 @@ class	PrettyPrint( superclass.MetaPrettyPrinter ):
 		octothorpe = line.find( '#' )
 		if octothorpe > -1:
 			line = line[:octothorpe]
-		tokens = line.split()
-		if len(tokens) > 0:
-			name = tokens[0]
-			if len(tokens) == 1:
-				value = []
-			else:
-				value = tokens[1:]
-			self.max_name = max( self.max_name, len(name) )
-			self.tokens.append( ( name, ' '.join(value) ) )
+		if line.startswith( '[' ):
+			if self.name is not None:
+				self.feed.sort( key = lambda (x,y): x.lower() )
+				self.feeds.append( (self.name, self.max_name, self.feed) )
+			self._new_repo( line.strip()[1:-1] )
+		else:
+			tokens = line.split( '=', 1 )
+			if len(tokens) == 2:
+				name = tokens[0].strip()
+				value = tokens[1].strip()
+				self.max_name = max( self.max_name, len(name) )
+				self.feed.append( (name, value) )
 		return
 
 	def	finish( self ):
-		fmt = '%%%ds %%s' % self.max_name
-		self.tokens.sort( key = lambda (n,v) : n.lower() )
-		for name,value in self.tokens:
-			print fmt % (name, value)
+		self.feeds.sort( key = lambda (n,l,f) : n.lower() )
+		for (name, max_name, entries) in self.feeds:
+			print '[%s]' % name
+			fmt = '%%%ds = %%s' % max_name
+			for (id,value) in entries:
+				print fmt % ( id, value )
 		return
