d1ffa2cf8592fac0a2e207498b2ceea1f5b16e2d
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2013-04-05 10:06:17 -0500

    Added variable support to crontab processing.

--- src/gpp/plugins/crontab-plugin.py
+++ src/gpp/plugins/crontab-plugin.py
@@ -20,6 +20,7 @@ class	PrettyPrint( superclass.MetaPrettyPrinter ):
 
 	def	_prepare( self ):
 		self.entries = []
+		self.vars    = []
 		self.widths  = {}
 		return
 
@@ -30,26 +31,40 @@ class	PrettyPrint( superclass.MetaPrettyPrinter ):
 
 	def	next_line( self, line ):
 		line = line.split( '#', 1 )[0].strip()
-		tokens = line.split()
-		if len(tokens) >= 6:
-			tokens[5] = tokens[5:]
-			for i in xrange( 0, 6 ):
-				try:
-					self.widths[i] = max( self.widths[i], len(tokens[i]) )
-				except:
-					self.widths[i] = len(tokens[i])
-			self.entries.append( tokens[:6] )
+		if line.find( '=' ) > -1:
+			tokens = line.split( '=', 1 )
+			self.vars.append( [ tokens[0].strip(), tokens[1].strip() ] )
+		else:
+			tokens = line.split()
+			if len(tokens) >= 6:
+				tokens[5] = tokens[5:]
+				for i in xrange( 0, 6 ):
+					try:
+						self.widths[i] = max( self.widths[i], len(tokens[i]) )
+					except:
+						self.widths[i] = len(tokens[i])
+				self.entries.append( tokens[:6] )
 		return
 
 	def	report( self, final = False ):
+		if len(self.vars) > 0:
+			for (n,v) in sorted(self.vars):
+				print '%s=%s' % (n, v)
 		if len(self.entries) > 0:
 			fmt = ''
 			sep = ''
 			for i in xrange( 0, 6 ):
-				fmt = fmt + ('%s%%ds' % (sep, self.widths[i]))
+				fmt = fmt + ('%s%%%ds' % (sep, self.widths[i]))
 				sep = '  '
 			self.entries.sort()
 			for tokens in self.entries:
-				print fmt % tokens
+				print fmt % (
+					tokens[0],
+					tokens[1],
+					tokens[2],
+					tokens[3],
+					tokens[4],
+					' '.join(tokens[5])
+				)
 		self._prepare()
 		return
