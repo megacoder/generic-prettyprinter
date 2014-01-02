114bdd91a548d6182a6d3b9c363f6120f85925bd
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2014-01-02 13:52:49 -0600

    Don't need no steenkin' extras

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
+# vim: noet sw=4 ts=4
 
 VERSION	= '1.0.2'
 
 from	distutils.core			import	setup
 import	glob
 import	os
-from	DistUtilsExtra.command	import	*
 
 setup(
-	name             ='generic_prettyprinter',
+	name             = 'generic_prettyprinter',
 	version          = VERSION,
-	description      ='Generic Pretty Printer, using plugins',
-	author           ='Tommy Reynolds',
-	author_email     ='Tommy.Reynolds@MegaCoder.com',
-	license          ='GPL',
-	url              ='http://www.MegaCoder.com',
+	description      = 'Generic Pretty Printer, using plugins',
+	author           = 'Tommy Reynolds',
+	author_email     = 'Tommy.Reynolds@MegaCoder.com',
+	license          = 'GPL',
+	url              = 'http://www.MegaCoder.com',
 	long_description =open('README.txt').read(),
-	packages         =['generic_prettyprinter' ],
+	packages         =[ 'generic_prettyprinter' ],
 	package_dir      ={ 'generic_prettyprinter':'src/gpp' },
 	package_data     ={ 'generic_prettyprinter': [ 'plugins/*.py', '*.txt', '*.html' ] },
-	scripts			=[ 'scripts/gpp' ],
+	scripts          =[ 'scripts/gpp' ],
 )
