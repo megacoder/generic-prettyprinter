25537b2d7955b668c62f50345444cb782984d7dd
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2014-05-31 12:31:14 -0500

    Perhaps a better-formatted output filtering.

--- generic_prettyprinter/plugins/oswbb-heartbeat-plugin.py
+++ generic_prettyprinter/plugins/oswbb-heartbeat-plugin.py
@@ -19,35 +19,44 @@ class	PrettyPrint( superclass.MetaPrettyPrinter ):
 		super( PrettyPrint, self ).__init__()
 		return
 
-	def	process( f = sys.stdin ):
-		last = None
-		old_delta = None
-		fmt = '%a %b %d %H:%M:%S %Y'
-		print '#  Day Mon Dy HH:MM:SS YYYY  Delta'
-		print '#  --- --- -- -------- ---- -------'
-		for line in f:
-			if not line.startswith( 'oswbb heartbeat' ): continue
-			tokens = line.rstrip().split( ':', 1 )
+	def	begin_file( self, fn ):
+		super( PrettyPrint, self ).begin_file()
+		self.old_delta = None
+		self.last      = None
+		self.println(
+			'#  Day Mon Dy HH:MM:SS YYYY  Delta'
+		)
+		self.println(
+			'#  --- --- -- -------- ---- -------'
+		)
+		return
+
+	def	next_line( s ):
+		if line.startswith( 'oswbb heartbeat:' ):
+			tokens = line.split( ':', 1 )
 			if len( tokens ) == 2:
 				parts = tokens[1].split()
 				if len(parts[2]) != 2:
 					parts[2] = '0' + parts[2]
 				when = ' '.join( parts[0:4] + [parts[-1]] )
 				try:
-					dt = datetime.datetime.strptime( when, fmt )
+					dt = datetime.datetime.strptime(
+						when,
+						'%a %b %d %H:%M:%S %Y'
+					)
 				except Exception, e:
-					raise e
-					continue
-				if last is None:
-					last = dt
-				delta = dt - last
+					self.println( e )
+					return
+				if self.last is None:
+					self.last = dt
+				delta = dt - self.last
 				if old_delta is None:
 					old_delta = delta
-				if delta != old_delta:
-					flag = '- '
-				else:
-					flag = '  '
-				print '%s %s %s' % ( flag, when, delta )
-				last = dt
-				old_delta = delta
+				print '%s %s %s' % (
+					'-' if flag else ' ',
+					tokens[1],
+					delta
+				)
+				self.last = dt
+				self.old_delta = delta
 		return
