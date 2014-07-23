f46030c08d64018450e36d847ac6a0e946284383
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2014-07-23 03:36:31 -0500

    Scaffolding for 'help' event to come.

--- src/__init__.py
+++ src/__init__.py
@@ -29,18 +29,19 @@ class GenericPrettyPrinter( object ):
         # Allow plugin to figure out where its files are
         if n < 1:
             names = o.own_glob()
+        if names:
             n = len( names )
-        if n < 1:
-            # Give plugin a chance to handle its no-file method
-            o.do_open_file()
-        else:
-            # Iterate through the names
-            o.advise( argc = n )
-            for name in names:
-                if name == '-':
-                    o.do_open_file()
-                else:
-                    o.do_name( name )
+            if n < 1:
+                # Give plugin a chance to handle its no-file method
+                o.do_open_file()
+            else:
+                # Iterate through the names
+                o.advise( argc = n )
+                for name in names:
+                    if name == '-':
+                        o.do_open_file()
+                    else:
+                        o.do_name( name )
         o.finish()
         return False
 
@@ -52,48 +53,60 @@ class GenericPrettyPrinter( object ):
             kind = os.path.basename( sys.argv[0] )[:-3]
         p = optparse.OptionParser(
             description = """Generic pretty-printer""",
-            usage = '%prog [-o ofile] [-t type] [file..]',
-            prog = 'gpp'
+            usage       = '%prog [-o ofile] [-t type] [file..]',
+            prog        = 'gpp'
         )
         p.add_option(
             '-o',
             '--out',
-            action='store',
-            type='string',
-            dest='ofile',
-            help='Output written to file; defaults to stdout.',
-            metavar='file'
+            action  = 'store',
+            type    = 'string',
+            dest    = 'ofile',
+            default =  None,
+            help    = 'Output written to file; defaults to stdout.',
+            metavar = 'file'
         )
         p.add_option(
             '-t',
             '--type',
-            action='store',
-            type='string',
-            dest='kind',
-            help='kind of pretty-printer desired; defaults to text.',
-            metavar='file'
-        )
-        p.set_defaults(
-            ofile = None,
-            kind = "text"
+            action  ='store',
+            type    ='string',
+            default = 'text',
+            dest    ='kind',
+            help    ='kind of pretty-printer desired; defaults to text.',
+            metavar ='file'
         )
         opts, args = p.parse_args()
         # Here we go...
-        dll_name = '%s-plugin' % opts.kind
+        if opts.kind == 'help':
+            retval = False
+        else:
+            dll_name = '%s-plugin' % opts.kind
+            # DEBUG print >>sys.stderr, 'Loading module %s' % dll_name
+            try:
+                dll = __import__(dll_name)
+            except Exception, e:
+                print >>sys.stderr, 'No prettyprinter for "%s".' % opts.kind
+                print >>sys.stderr, e
+                return True
+            if opts.ofile is not None:
+                try:
+                    sys.stdout = open( opts.ofile, 'wt' )
+                except Exception, e:
+                    print >>sys.stderr, 'Cannot open "%s" for writing.' % opts.ofile
+                    return True
+            retval = self.doit( Obj = dll.PrettyPrint, names = args )
+        return retval
+
+    def do_one_module( self, kind = 'help', args = [] ):
+        dll_name = '%s-plugin' % kind
         # DEBUG print >>sys.stderr, 'Loading module %s' % dll_name
         try:
             dll = __import__(dll_name)
         except Exception, e:
-            print >>sys.stderr, 'Sorry, no prettyprinter for "%s".' % opts.kind
-            print >>sys.stderr, 'What follows is the python error indicator we received:'
+            print >>sys.stderr, 'No prettyprinter for "%s".' % opts.kind
             print >>sys.stderr, e
             return True
-        if opts.ofile is not None:
-            try:
-                sys.stdout = open( opts.ofile, 'wt' )
-            except Exception, e:
-                print >>sys.stderr, 'Cannot open "%s" for writing.' % opts.ofile
-                return True
         retval = self.doit( Obj = dll.PrettyPrint, names = args )
         return retval
 
