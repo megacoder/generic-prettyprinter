d3b2e93a9620ff39997751b6aaa5d86c6160743e
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2015-09-04 14:00:55 -0500

    Drop the RPM nonsense because that RPM is locked to a particular python version.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 if [ $# -eq 0 ]; then
-	# set -- bdist --format=bztar
+	set -- bdist --format=bztar
 	# set -- bdist --format=gztar
-	set -- bdist --format=rpm
+	# set -- bdist --format=rpm
 fi
 (
 	set +x
