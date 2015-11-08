1dc22b6d9aacc66cf06cb9aca11b623a217e6a00
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2015-11-08 13:00:36 -0600

    Process /etc/nsswitch.* content

--- src/nsswitch-plugin.py
+++ src/nsswitch-plugin.py
@@ -1,52 +1,38 @@
 #!/usr/bin/python
-# vim: sw=4 ts=4 noet
 
 import	os
 import	sys
-import	stat
-from	superclass	import	MetaPrettyPrinter
+import  superclass
+import  align
 
-class	PrettyPrint( MetaPrettyPrinter ):
+class   PrettyPrint( superclass.MetaPrettyPrinter ):
 
-	NAME = 'nsswitch-pp'
-	DESCRIPTION = """Display /etc/nsswitch.conf configuration files in canonical format."""
+    NAME = 'nsswitch-pp'
+    DESCRIPTION = """Display nsswitch.conf in canonical style."""
 
-	def	__init__( self ):
-		super( PrettyPrint, self ).__init__()
-		return
+    def __init__( self ):
+        super( PrettyPrint, self ).__init__()
+        self.pre_open_file()
+        return
 
-	def	reset( self ):
-		super( PrettyPrint, self ).reset()
-		self._setup()
-		return
+    def pre_open_file( self ):
+        self.lines   = []
+        return
 
-	def	_setup( self ):
-		self.settings = []
-		self.max_name = 7
-		return
+    def next_line( self, line ):
+        # Drop comments
+        parts = line.split( '#', 1 )[0].strip().split()
+        if len( parts ) > 0:
+            # Save non-blank lines
+            self.lines.append( parts )
+        return
 
-	def	next_file( self, name ):
-		super( PrettyPrint, self ).next_file( name )
-		self._setup()
-		return
-
-	def	end_file( self, name ):
-		self.report()
-		self._setup()
-		super( PrettyPrint, self ).end_file( name )
-		return
-
-	def	next_line( self, line ):
-		tokens = line.split( '#', 1 )[0].strip().split(':', 1)
-		if len(tokens) == 2:
-			name  = tokens[0].strip()
-			value = ' '.join( tokens[1].strip().split() )
-			self.max_name = max( self.max_name, len(name) )
-			self.settings.append( [name, value] )
-		return
-
-	def	report( self, final = False ):
-		fmt = '%%-%ds: %%s' % self.max_name
-		for name,value in sorted( self.settings ):
-			self.println( fmt % (name, value) )
-		return
+    def report( self, final = False ):
+        if not final:
+            self.lines.sort( key = lambda key: key )
+            a = align.align()
+            for parts in self.items:
+                a.add( parts )
+            for parts in a.get_items():
+                self.println( ' '.join( parts ) )
+        return
