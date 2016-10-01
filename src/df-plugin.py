4e5c1ae43ce6c17a4e23605a3a7d502a906038c7
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2016-10-01 15:23:12 -0500

    Handle folded input lines better

--- src/df-plugin.py
+++ src/df-plugin.py
@@ -18,29 +18,32 @@ class   PrettyPrint( superclass.MetaPrettyPrinter ):
     def pre_open_file( self ):
         self.items   = []
         self.headers = None
+        self.pending = None
         return
 
     def next_line( self, line ):
-        parts = line.split( '#', 1 )[0].strip().split()
+        parts = map(
+            str.strip,
+            line.split( '#', 1 )[0].split()
+        )
+        # If the partition name was very, very long, its
+        # line was folded, so unfold it here
+        if self.pending:
+            parts += self.pending
+            self.pending = None
         if self.headers:
-            if len( parts ) > 0:
-                self.items.append( parts )
+            N = len( parts )
+            if N:
+                if N < 6:
+                    self.pending = parts
+                else:
+                    self.items.append( parts )
         else:
             if line.startswith( 'Filesystem' ):
                 # Coalesce 'Mounted' 'on' into 'Mounted on'
                 parts[-2] = ' '.join( parts[-2:] )
                 # Save all but isolated 'on', which is the final arg
                 self.headers = parts[ : -1 ]
-        return
-
-    def _print_tuples( self, tuples ):
-        sep = ''
-        line = ''
-        for i in xrange( 0, len(tuples) ):
-            fmt = '%%s%%-%ds' % self.widths[i]
-            line += fmt % ( sep, tuples[i] )
-            sep = '  '
-        self.println( line )
         return
 
     def report( self, final = False ):
