a9fea9ac7195bfae2510ec04f912f85fdba482c5
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2016-12-05 17:49:40 -0600

    Let auto-align titles inherit justification of their column data

--- src/align.py
+++ src/align.py
@@ -7,21 +7,86 @@ import	re
 
 class	Align( object ):
 
+	def	_left( self, key, value ):
+	   width = self.widths.get( key, 7 )
+	   fmt = '{{0:<{0}}}'.format( width )
+	   return fmt.format( value )
+
+	def	_right( self, key, value ):
+		width = self.widths.get( key, 7 )
+		fmt = '{{0:>{0}}}'.format( width )
+		return fmt.format( value )
+
+	def	_center( self, key, value, pad = ' ' ):
+		width = self.widths.get( key, 7 )
+		fmt = '{{0:>{0}}}'.format( width )
+		extra = (width - len( value )) / 2
+		return fmt.format( value + (pad * extra) )
+
+	def	_auto( self, key, value ):
+		if self.numeric[ key ]:
+			return self._right( key, value )
+		if self.want_lj:
+			return self._left( key, value )
+		return self._right( key, value )
+
 	def	__init__( self, lj = False, titles = 0 ):
-		self.titles  = titles
-		self.want_lj = lj
-		self.widths  = dict()
-		self.numeric = dict()
-		self.re      = re.compile(
+		self.align_column = dict()
+		self.align_title  = dict()
+		self.items        = []
+		self.nItems       = 0
+		self.numeric      = dict()
+		self.titles       = titles
+		self.want_lj      = lj
+		self.widths       = dict()
+		self.align_map = dict(
+			a = self._auto,
+			c = self._center,
+			l = self._left,
+			r = self._right
+		)
+		self.re           = re.compile(
 			# Signed/unsigned integer|floating|scientific
 			r'(^[-+]?[0-9]{1,}([.][0-9]{1,})?([Ee][-+]?[0-9]{1,})?)$'
 		)
-		self.items   = []
-		self.nItems  = 0
 		return
 
-	def	get_widths( self ):
-		return self.widths
+	def	set_alignment( self, how = 'a' ):
+		i = 0
+		for k in range( self.get_columns() ):
+			key = str( k )
+			self.align_column[ key ] = self.align_map.get(
+				how[i],
+				self._auto
+			)
+			i = (i + 1) % len( how )
+		return
+
+	def	set_title_alignment( self, how = 'a' ):
+		i = 0
+		for k in range( self.get_columns() ):
+			key = str( k )
+			self.align_title[ key ] = self.align_map.get(
+				how[ i ],
+				self._auto
+			)
+			i = (i + 1) % len( how )
+		return
+
+	def	show_alignment( self, out = sys.stdout ):
+		print 'Column Alignment'
+		for key in sorted( self.align_column ):
+			print >>out, '{0}\t{1}'.format( key, self.align_column[key] )
+		return
+
+	def	show_title_alignment( self, out = sys.stdout ):
+		print 'Title Alignment'
+		for key in sorted( self.align_title ):
+			print >>out, '{0}\t{1}'.format( key, self.align_title[key] )
+		return
+
+	def	get_columns( self ):
+		return len( self.widths.keys() )
 
 	def	add( self, l ):
 		L = len( l )
@@ -47,12 +112,21 @@ class	Align( object ):
 		return
 
 	def	get_items( self, titles = 0, sort = lambda x : 0 ):
+		# Let auto columns inherit the justification of their column data
+		for key in self.align_title.keys():
+			if self.align_title[key] == self._auto:
+				self.align_title[key] = self.align_column.get(
+					key,
+					self._auto
+				)
 		# Titles are first
 		for H,tokens in enumerate( self.items[:self.titles] ):
 			columns = []
 			for i,token in enumerate( tokens ):
-				fmt = '{{0:>{0}}}'.format( self.widths[ str(i) ] )
-				columns.append( fmt.format( token ) )
+				key = str( i )
+				columns.append(
+					self.align_title.get( key, self._auto )( key, token )
+				)
 			yield H,columns
 		if sort == True:
 			sort = lambda x : x
@@ -62,33 +136,32 @@ class	Align( object ):
 			columns = []
 			for i,token in enumerate( tokens ):
 				key = str( i )
-				max_width = self.widths[ key ]
-				justification = '>'
-				if self.numeric[ key ]:
-					justification = '>'
-				else:
-					if self.want_lj:
-						justification = '<'
-				# Add formatted item
-				fmt = r'{{0:{0}{1}}}'.format( justification, max_width )
-				columns.append( fmt.format( token ) )
+				columns.append(
+					self.align_column.get( key, self._auto )( key, token )
+				)
 			yield self.titles+N,columns
 		return
 
 if __name__ == '__main__':
 	a = Align( lj = True, titles = 1 )
-	a.add( [ 'First', 'Second', 'Third', 'Fourth' ] )
+	a.add( [ 'First', 'Second', '3rd', 'Fourth' ] )
 	a.add( [ 1.2,22,3.33, 'astro' ] )
 	a.add( [ 1,22,333, 'aSTRo' ] )
 	a.add( [ -44,5,6, 'rubble' ] )
 	a.add( [ 321,'abc','def', 123 ] )
 	print 'Plain:'
+	a.set_title_alignment( 'rlca' )
+	a.set_alignment( 'cccc' )
 	for i,items in a.get_items():
 		print 'Line {0}->|{1}|'.format( i+1, '|'.join( items ) )
 	print 'Sorted'
+	a.set_title_alignment( 'llll' )
+	a.set_alignment( 'aaaa' )
 	for i,items in a.get_items( sort = True):
 		print 'Line {0}->|{1}|'.format( i+1, '|'.join( items ) )
 	print 'Weird'
+	a.set_title_alignment( 'a' )
+	a.set_alignment( 'a' )
 	import random
 	how = lambda x : random.random()
 	for i,items in a.get_items( sort = how ):
