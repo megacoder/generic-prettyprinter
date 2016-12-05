1c1958738688efd32886ce7f9ef7b950a9538c1f
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2016-12-05 14:09:36 -0600

    Use align.Align() class with custom sorting.

--- src/arp-plugin.py
+++ src/arp-plugin.py
@@ -6,6 +6,7 @@ import	sys
 import	string
 import	superclass
 import	re
+import	align
 
 class	PrettyPrint( superclass.MetaPrettyPrinter ):
 
@@ -18,27 +19,23 @@ class	PrettyPrint( superclass.MetaPrettyPrinter ):
 		return
 
 	def	pre_begin_file( self ):
-		self.need_title = True
-		self.items      = []
-		self.title      = ""
+		self.items = align.Align( titles = 1 )
 		return
 
 	def	next_line( self, line ):
-		line = line.strip()
-		if len(line) > 0:
-			if self.need_title:
-				self.need_title = False
-				self.title = line
-			else:
-				ip = line.split()[0].split( '.' )
-				# self.items.append( (ip, line) )
-				self.items.append( ([ int(x) for x in ip], line) )
+		tokens = map(
+			str.strip,
+			line.split()
+		)
+		Ntokens = len( tokens )
+		if Ntokens >= 4:
+			# If no netmask field, insert an empty one
+			if Ntokens == 5:
+				tokens = tokens[:4] + [ "" ] + tokens[4:]
+			self.items.add( tokens )
 		return
 
 	def	report( self, final = False ):
-		if len(self.items) > 0:
-			self.items.sort( key = lambda (ip,line): ip )
-			print self.title
-			for (ip,line) in self.items:
-				print line
+		for _,items = self.items.get_items():
+			print ' '.join( items )
 		return
