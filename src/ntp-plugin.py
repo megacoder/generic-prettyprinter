4de7998859e07f59b3941e7052f0f114d4560774
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2017-04-08 22:02:42 -0500

    Use dictionaries to make dividing into categories easier.

--- src/ntp-plugin.py
+++ src/ntp-plugin.py
@@ -14,62 +14,42 @@ class   PrettyPrint( superclass.MetaPrettyPrinter ):
         super( PrettyPrint, self ).__init__()
         return
 
-    def reset( self ):
-        super( PrettyPrint, self ).reset()
-        self._prepare()
-        return
-
-    def _prepare( self ):
-        self.tokens  = []
-        return
-
     def ignore( self, name ):
         return not name.endswith( '.conf' )
 
-    def begin_file( self, name ):
-        super( PrettyPrint, self ).begin_file( name )
-        self._prepare()
+    def pre_begin_file( self, name = fn ):
+        super( PrettyPrint, self ).pre_begin_file( name )
+        self.groups = dict()
         return
 
     def next_line( self, line ):
         tokens = line.split( '#', 1 )[0].split()
         if len(tokens) > 0:
-            self.tokens.append( tokens )
+            key = tokens[0].lower()
+            self.groups[key] = self.groups(key,list()).append(tokens)
         return
 
     def report( self, final = False ):
+        if final: return
         # Space groups of similar lines similarly
-        last_kind = None
-        lines = []
-        for tokens in sorted( self.tokens, key = lambda t : t[0].lower() ):
-            kind = tokens[0].lower()
-            if kind != last_kind:
-                if len(lines) > 0:
-                    self._print_group( lines )
-                    lines = []
-                lines.append( tokens )
-                last_kind = kind
-        if len(lines) > 0:
-            self._print_group( lines )
-        self._prepare()
-        return
-
-    def _print_group( self, lines ):
-        # Calculate column widths
-        widths = {}
-        for tokens in lines:
-            for i in xrange( 0, len(tokens) ):
-                try:
-                    widths[i] = max( widths[i], len(tokens[i]) )
-                except:
-                    widths[i] = max( 12, len(tokens[i]) )
-        # Dump group using fixed-width columns
-        for tokens in lines:
-            s = ""
-            sep = ""
-            for i in xrange( 0, len(tokens) ):
-                fmt = '%%s%%-%ds' % widths[i]
-                s += fmt % (sep, tokens[i] )
-                sep = " "
-            self.println( s )
+        for key in sorted( self.groups ):
+            widths = dict()
+            for tokens in sorted( self.groups[key] ):
+                for i in range( len( tokens ) ):
+                    widths[i] = max(
+                        widths.get( i, 0 ),
+                        len( tokens )
+                    )
+            # Build the column formats
+            fmts = dict()
+            for i in len( widths ):
+                fmts[i] = '{{0:{0}}}'.format( widths[i] )
+            # Output this group
+            self.println()
+            for tokens in sorted( self.groups[key] ):
+                cols = map(
+                    lambda i : fmts[i].format( tokens[i] ),
+                    range( len( tokens ) )
+                )
+                self.println( ' '.join( cold ) )
         return
