c12b2fcf560fcfb19ab173418f586d14a792ad07
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2017-04-08 22:37:43 -0500

    Typo on pre_begin_file() method argument

--- src/ntp-plugin.py
+++ src/ntp-plugin.py
@@ -1,55 +1,56 @@
 #!/usr/bin/python
+# vim: noet sw=4 ts=4
 
-import  os
-import  sys
-import  math
-import  superclass
+import	os
+import	sys
+import	math
+import	superclass
 
-class   PrettyPrint( superclass.MetaPrettyPrinter ):
+class	PrettyPrint( superclass.MetaPrettyPrinter ):
 
-    NAME = 'ntp-pp'
-    DESCRIPTION="""Output /etc/ntp.conf in canonical form."""
+	NAME = 'ntp-pp'
+	DESCRIPTION="""Output /etc/ntp.conf in canonical form."""
 
-    def __init__( self ):
-        super( PrettyPrint, self ).__init__()
-        return
+	def __init__( self ):
+		super( PrettyPrint, self ).__init__()
+		return
 
-    def ignore( self, name ):
-        return not name.endswith( '.conf' )
+	def ignore( self, name ):
+		return not name.endswith( '.conf' )
 
-    def pre_begin_file( self, name = fn ):
-        super( PrettyPrint, self ).pre_begin_file( name )
-        self.groups = dict()
-        return
+	def pre_begin_file( self, fn = None ):
+		self.groups = dict()
+		return
 
-    def next_line( self, line ):
-        tokens = line.split( '#', 1 )[0].split()
-        if len(tokens) > 0:
-            key = tokens[0].lower()
-            self.groups[key] = self.groups(key,list()).append(tokens)
-        return
+	def next_line( self, line ):
+		tokens = line.split( '#', 1 )[0].split()
+		if len(tokens) > 0:
+			key = tokens[0].lower()
+			self.groups[key] = self.groups(key,list()).append(tokens)
+		return
 
-    def report( self, final = False ):
-        if final: return
-        # Space groups of similar lines similarly
-        for key in sorted( self.groups ):
-            widths = dict()
-            for tokens in sorted( self.groups[key] ):
-                for i in range( len( tokens ) ):
-                    widths[i] = max(
-                        widths.get( i, 0 ),
-                        len( tokens )
-                    )
-            # Build the column formats
-            fmts = dict()
-            for i in len( widths ):
-                fmts[i] = '{{0:{0}}}'.format( widths[i] )
-            # Output this group
-            self.println()
-            for tokens in sorted( self.groups[key] ):
-                cols = map(
-                    lambda i : fmts[i].format( tokens[i] ),
-                    range( len( tokens ) )
-                )
-                self.println( ' '.join( cold ) )
-        return
+	def report( self, final = False ):
+		if final: return
+		# Space groups of similar lines similarly
+		for key in sorted( self.groups ):
+			# One pass to calculate the widths of this group columns
+			widths = dict()
+			for tokens in self.groups[key]:
+				widths = map(
+					lambda i: max( widths.get(i,0), len(tokens[i])),
+					range( len(tokens) )
+				)
+			# Build the column formats
+			fmts = map(
+				lambda i: '{{0:{0}}}'.format( widths[i] ),
+				range( len( widths ) )
+			)
+			# Output this group
+			self.println()
+			for tokens in sorted( self.groups[key] ):
+				cols = map(
+					lambda i : fmts[i].format( tokens[i] ),
+					range( len( tokens ) )
+				)
+				self.println( ' '.join( cold ) )
+		return
