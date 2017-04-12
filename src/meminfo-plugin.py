dbc0c4e9f8b2193122215c92ca7bc3ab60562416
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2017-04-12 10:50:23 -0500

    Save values in a dict() to make doing the math easier.

--- src/meminfo-plugin.py
+++ src/meminfo-plugin.py
@@ -15,174 +15,155 @@ class	PrettyPrint( superclass.MetaPrettyPrinter ):
 		super( PrettyPrint, self ).__init__()
 		return
 
-	def	reset( self ):
-		super( PrettyPrint, self ).reset()
-		self._prepare()
-		return
-
-	def	_prepare( self ):
-		self.lines = []
-		self.maxfield = 12
-		return
-
-	def	begin_file( self, name ):
-		super( PrettyPrint, self ).begin_file( name )
-		self._prepare()
+	def	pre_begin_file( self, fn = None ):
+		super( PrettyPrint, self ).pre_begin_file( fn )
+		self.entries = dict()
+		self.ordered_keys = []
 		return
 
 	def	next_line( self, line ):
-		tokens = line.rstrip().split( ':' )
-		if len(tokens) > 0:
-			field = tokens[0].strip()
-			value = tokens[1].strip()
-			self.maxfield = max( self.maxfield, len(field) )
-			values = value.split()
-			# Some entries have no specific units specified
-			if not value.endswith( 'kB' ):
-				# Null units to make the columns line up
-				values.append( '  ' )
-			if len(values) >= 2 :
-				self.lines.append( (field, values) )
-		return
-
-	def	_begin_notes( self ):
-		self.first = True
-		return
-
-	def	_addto_notes( self, msg, val = None, fmt = None ):
-		if self.first:
-			self.first = False
-			self.println()
-			self.println( 'Observations about these values.' )
-			self.println( '--------------------------------' )
-		if val:
-		    if not fmt:
-			fmt = '{0:>14s}: {1}'
-		    self.println(
-			fmt.format(
-			    val,
-			    msg
+		tokens = map(
+			str.strip,
+			line.rstrip().split( ':' )
+		)
+		N = len( tokens )
+		if N == 2:
+			field  = tokens[0]
+			detail = tokens[1].split()
+			value  = detail[0]
+			rest   = detail[1:]
+			self.ordered_keys.append( field )
+			self.entries[ field ] = dict(
+				name  = field,
+				value = value,
+				rest  = rest,
+				notes = []
 			)
-		    )
-		else:
-		    if not fmt:
-			fmt = '{0}'
-		    self.println(
-			fmt.format( msg )
-		    )
 		return
 
-	def	_end_notes( self ):
+	def	_get_entry( self, name, default = 0 ):
+		return int(self.entries[name]['value']) if name in self.entries else default
+
+	def	_add_note( self, key, s ):
+		n = self.footnote( s )
+		self.entries[key]['notes'].append( str(n) )
+		return
+
+	def	_infer_overcommit( self ):
+		# Check for memory overcommit
+		CommitLimit  = float( self._get_entry( 'CommitLimit' ) )
+		Committed_AS = float( self._get_entry( 'Comitted_AS' ) )
+		if CommitLimit > 0.0:
+			ratio = (Committed_AS / CommitLimit) * 100.0
+			if ratio > 0.0:
+				self._add_note(
+					'Committed_AS',
+					'Memory commit ratio = {0:.2f}'.format( ratio )
+				)
+			if ratio > 100.0:
+				self._add_note(
+					'Committed_AS',
+					'*** WORKING SET TOO LARGE: Committed_AS > CommitLimit ***'
+				)
+		return
+
+	def	_infer_hugepages( self ):
+		managed_memory  = self._get_entry( 'MemTotal' )
+		managed_memory -= self._get_entry( 'HugePages_Total' )
+		min_free_kbytes = int(
+			( math.sqrt( managed_memory ) * 4.0 ) + 0.5
+		)
+		self._add_note(
+			'MemTotal',
+			'Default vm.min_free_kbytes = {0}'.format(
+				min_free_kbytes
+			)
+		)
+		recommended_min_free_kbytes = int(
+			(managed_memory * 0.005) + 0.5
+		)
+		self._add_note(
+			'MemTotal',
+			'Recommended vm.min_free_kbytes = {0}'.format(
+				recommended_min_free_kbytes
+			)
+		)
+		return
+
+	def	_infer_wasted_memory( self ):
+		hp = self._get_entry( 'HugePages_Total' )
+		hp -= self._get_entry( 'HugePages_Rsvd' )
+		wasted = hp * self._get_entry( 'Hugepagesize' )
+		if wasted > 0:
+			self._add_note(
+				'HugePages_Total',
+				'Wasted physical memory {0}'.format( wasted )
+			)
+		return
+
+	def	_infer_avail_memory( self ):
+		avail = self._get_entry( 'MemTotal' ) - (
+			self._get_entry( 'HugePages_Total' ) *
+			self._get_entry( 'Hugepagesize' )
+		)
+		self._add_note(
+			'MemTotal',
+			'Physical memory not in HugePages: {0}'.format(
+				avail
+			)
+		)
+		return
+
+	def	_infer_dom0_memory( self ):
+		dom0mem = (
+			502 + int(
+				(self._get_entry( 'MemTotal' ) / 1024.0 * 0.0205) + 0.5
+			)
+		)
+		self._add_note(
+			'MemTotal',
+			'Recommended memory if is dom0: {0}'.format(
+				dom0mem
+			)
+		)
 		return
 
 	def	report( self, final = False ):
-		if len(self.lines) == 0:
-			self._prepare()
-			return
-		maxvalue = 12
-		for (field,values) in self.lines:
-			maxvalue = max( maxvalue, len(values[0]) )
-		ffmt = '%%-%ds' % (self.maxfield+1)
-		vfmt = '%%%ds %%s' % maxvalue
-		fmt = ffmt + '	' + vfmt
-		observed = {}
-		for (field, values) in self.lines:
-			observed[field] = int(values[0])
-			self.println( fmt % (field + ':', values[0], values[1]) )
-		self._begin_notes()
-		try:
-			if observed['Committed_AS'] > observed['CommitLimit']:
-				self._addto_notes(
-					'Committed_AS(%d)>CommitLimit(%d) suggests system overload.' % (
-						observed['Committed_AS'],
-						observed['CommitLimit']
-					)
-				)
-		except Exception, e:
-			self._addto_notes(
-				'Could not check CommitLimit'
+		if final: return
+		self._infer_overcommit()
+		self._infer_hugepages()
+		self._infer_wasted_memory()
+		self._infer_avail_memory()
+		self._infer_dom0_memory()
+		#
+		keys = self.entries.keys()
+		key_width = max(
+			map(
+				len,
+				keys
 			)
-			self._addto_notes(
-				str(observed)
+		)
+		value_width = max(
+			map(
+				len,
+				[
+					self.entries[key]['value'] for key in self.entries
+				]
 			)
-			self._addto_notes( e )
-		try:
-			managed_memory = float( observed[ 'MemTotal' ] )
-			if 'HugePages_Total' in observed:
-			    managed_memory -= observed['HugePages_Total']
-			min_free_kbytes = int(
-			    (math.sqrt( managed_memory ) * 4.0) + 0.5
-			)
-			recommended_min_free_kbytes = int(
-			    (managed_memory * 0.005) + 0.5
-			)
-			self._addto_notes(
-			    'Default vm.min_free_kbytes',
-			    val = '{0} kB'.format( min_free_kbytes )
-			)
-			self._addto_notes(
-			    'Recommended vm.min_free_kbytes',
-			    val = '{0} kB'.format(
-				recommended_min_free_kbytes
-			    )
-			)
-		except Exception, e:
-			self._addto_notes(
-				'Could not calculate default vm.min_free_kbytes'
-			)
-			self._addto_notes( e )
-		try:
-			if 'HugePages_Total' in observed:
-				hp = observed['HugePages_Total']
-				if 'HugePages_Rsvd' in observed:
-					hp -= observed['HugePages_Rsvd']
-				wasted_memory = hp * observed['Hugepagesize']
-				if wasted_memory > 0:
-				    self._addto_notes(
-					'Physical memory in unused HugePages',
-					val = '{0} kB ({1} pages)'.format(
-					    wasted_memory,
-					    hp
-					)
-				    )
-		except Exception, e:
-			self._addto_notes(
-				'Could not calculate wasted hugepages space.'
-			)
-			self._addto_notes( e )
-		try:
-			if 'HugePages_Total' in observed:
-				working_space = (
-					observed['MemTotal'] - (
-						observed['HugePages_Total'] * observed['Hugepagesize']
-					)
-				)
-				self._addto_notes(
-				    'Physical memory not in HugePages',
-				    val = '{0} kB'.format( working_space )
-				)
-		except Exception, e:
-			self._addto_notes(
-				'Could not calculate available physical memory.'
-			)
-			self._addto_notes( e )
-		try:
-			dom0mem = (
-				502 + int(
-					(observed['MemTotal'] / 1024.0 * 0.0205) + 0.5
+		)
+		fmt = '{{0:{0}s}} {{1:>{1}s}} {{2:2s}} {{3}}'.format(
+			key_width + 1,
+			value_width
+		)
+		#
+		self.println()
+		for key in self.ordered_keys:
+			self.println(
+				fmt.format(
+					'{0}:'.format( key ),
+					self.entries[key]['value'],
+					' '.join( self.entries[key]['rest'] ),
+					', '.join( self.entries[key]['notes'] )
 				)
 			)
-			self._addto_notes(
-			    'Recommended memory if dom0',
-			    val = '{0} MB'.format( dom0mem )
-			)
-		except Exception, e:
-			self._addto_notes(
-				'Could not calculate recommended dom0 partition size.'
-			)
-			self._addto_notes(
-				e
-			)
-		self._end_notes()
-		self._prepare()
 		return
