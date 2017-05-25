9c19b2651977fa69687f9d6379d36116d0897a63
Tommy Reynolds <Oldest.Software.Guy@gmail.com>
2017-05-25 17:01:09 -0500

    Switch from optargse to argparse, because argparse is the future.

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 # vim: et sw=4 ts=4
 
+import  argparse
+import  importlib
 import  os
 import  sys
-import  optparse
-import  importlib
 import  version
 
 class GenericPrettyPrinter( object ):
@@ -23,7 +23,10 @@ class GenericPrettyPrinter( object ):
             pattern = self.GLOB
         return glob.glob( pattern )
 
-    def _session( self, Handler, names = [] ):
+    def _session( self, Handler, names = [[]] ):
+        print 'names={0}'.format( names )
+        for name in names:
+            print 'name={0}'.format( name )
         handler = Handler()
         # Allow plugin to figure out where its files are
         if len( names ) == 0:
@@ -32,6 +35,11 @@ class GenericPrettyPrinter( object ):
         handler.start()
         handler.advise( names )
         for name in names:
+            print 'type(name)={0}, type(names)={1}'.format(
+                type(name),
+                type(names),
+            )
+            print 'filename to process: {0}'.format( name )
             handler.process( name )
         handler.finish()
         return False
@@ -42,48 +50,70 @@ class GenericPrettyPrinter( object ):
         return
 
     def main( self ):
-        print 'Generic prettyprinter (gpp) Version {0}'.format( version.Version )
+        # print 'Generic prettyprinter (gpp) Version {0}'.format( version.Version )
         sys.path.insert( 0, os.path.dirname( __file__ ) )
         # Intuit the kind of prettyprinter we want to be
         kind = 'text'
         if sys.argv[0].endswith( '-pp' ):
             kind = os.path.basename( sys.argv[0] )[:-3]
-        p = optparse.OptionParser(
-            description = """Generic pretty-printer""",
-            usage       = '%prog [-o ofile] [-t type] [file..]',
-            prog        = 'gpp',
-            version     = version,
+        p = argparse.ArgumentParser(
+            description     = """A modular pretty printer that is easy to
+            extend.""",
+            prog            = 'gpp',
+#           usage           = '%{prog} [-o ofile] [-t type] [file..]',
+            version         = version.Version,
+            formatter_class = argparse.ArgumentDefaultsHelpFormatter,
+            epilog = """Every attempt is made to provide a
+            correctness-preserving tansformation.  The content may look
+            different but correct input should result in correct output.
+            The idea is for functionally-equivalent input to produce
+            functionally-equivalent output so that two different file
+            organizations can be compared, even if the line formatting
+            differs or the line order differs."""
         )
-        p.add_option(
+        p.add_argument(
             '-D',
             '--debug',
-            action  = 'store',
-            type    = 'string',
+            action  = 'count',
             dest    = 'debug_level',
             default =  0,
             help    = 'Increase debug verbosity.'
         )
-        p.add_option(
+        p.add_argument(
+            'files',
+            metavar = 'FILE',
+#           action  = 'append',
+#           type    = list,
+            nargs   = '*',
+#           default = [],
+#           dest    = 'files',
+#           default = [],
+        )
+        fake_ofile = '{stdout}'
+        p.add_argument(
             '-o',
             '--out',
             action  = 'store',
-            type    = 'string',
+            type    = str,
             dest    = 'ofile',
-            default =  None,
-            help    = 'Output written to file; defaults to stdout.',
-            metavar = 'file'
+            default =  fake_ofile,
+            help    = 'output written to file',
+            metavar = 'PATH'
         )
-        p.add_option(
+        p.add_argument(
             '-t',
             '--type',
-            action  ='store',
-            type    ='string',
-            default = 'text',
-            dest    ='kind',
-            help    ='kind of pretty-printer desired; defaults to text.',
-            metavar ='file'
+            metavar  = 'TYPE',
+            action   = 'store',
+            type     = str,
+            default  = 'text',
+            dest     = 'kind',
+            help     = 'kind of pretty-printer desired',
+            required = True,
         )
-        opts, args = p.parse_args()
+        opts = p.parse_args()
+        if opts.ofile == fake_ofile:
+            opts.ofile = None
         # Here we go...
         module_name = '{0}-plugin'.format( opts.kind )
         try:
@@ -102,7 +132,7 @@ class GenericPrettyPrinter( object ):
             except Exception, e:
                 print >>sys.stderr, 'Cannot open "%s" for writing.' % opts.ofile
                 return True
-        retval = self._session( module.PrettyPrint, args )
+        retval = self._session( module.PrettyPrint, opts.files )
         return retval
 
 if __name__ == '__main__':
