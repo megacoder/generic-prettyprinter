a2105b5a3bd8293cd50ed4835c0e550ece991e2f
Tommy Reynolds <Oldest.Software.Guy@gmail.com>
2017-05-26 15:07:41 -0500

    Drop splat comments and blank lines.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python27
 # vim: noet sw=4 ts=4
 
-VERSION	= '1.0.57'
+VERSION	= '1.0.58'
 
 try:
 	# If this works, you can use eggs
---
+++ src/nocomment-plugin.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import  os
+import  sys
+import  math
+import  superclass
+
+class   PrettyPrint( superclass.MetaPrettyPrinter ):
+
+    NAME = 'nocomment-pp'
+    DESCRIPTION="""Drop splat comments from plain text."""
+
+    def __init__( self ):
+        super( PrettyPrint, self ).__init__()
+        return
+
+    def next_line( self, line ):
+        tokens = line.split( '#', 1 )
+        if len(tokens) and len(tokens[0]):
+            self.println( tokens[0] )
+        return
