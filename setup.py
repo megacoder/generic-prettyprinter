4829745f4e0237ab6ffb63aa0f1863ca82814ef5
Tommy Reynolds <Oldest.Software.Guy@gmail.com>
2018-01-20 21:56:49 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import	glob
 import	os
 
 NAME	= 'genpp'
-VERSION = '2.0.4'
+VERSION = '2.0.5'
 
 with open( '{0}/version.py'.format( 'genpp' ), 'w') as f:
 	print >>f, 'Version="{0}"'.format( VERSION )
