14ce93e845974cb9a4f3f6433d355942451abbf6
Tommy Reynolds <Oldest.Software.Guy@gmail.com>
2018-04-13 16:01:58 -0500

    Print structured network setup

--- genpp/ifcfg-plugin.py
+++ genpp/ifcfg-plugin.py
@@ -12,7 +12,6 @@ class   PrettyPrint( superclass.MetaPrettyPrinter ):
         super( PrettyPrint, self ).__init__()
         self.device    = None
         self.nics      = dict()
-        self.max_width = 7
         return
 
     def ignore( self, name ):
@@ -20,7 +19,12 @@ class   PrettyPrint( superclass.MetaPrettyPrinter ):
         return not name.endswith( '.conf' )
 
     def pre_begin_file( self, fn ):
-        self.nic = dict({ '_used' : False })
+        self.nic = dict({
+            '_used'  : False,
+            '_id'    : None,
+            'NAME'   : None,
+            'DEVICE' : None,
+        })
         return
 
     def next_line( self, line ):
@@ -36,112 +40,50 @@ class   PrettyPrint( superclass.MetaPrettyPrinter ):
             self.nic[name] = value
         return
 
+    def get_nic_id( self, nic ):
+        id = nic.get( '_id', None )
+        if not id: id = nic.get( 'NAME', None )
+        if not id: id = nic.get( 'DEVICE', None )
+        if not id: id = 'TBD'
+        return id
+
     def end_file( self, fn ):
-        if 'NAME' in self.nic:
-            name = self.nic[ 'NAME' ]
-        elif 'DEVICE' in self.nic:
-            name = self.nic[ 'DEVICE' ]
-        else:
-            name = 'ANONYMOUS'
-        self.nic[ 'NAME' ]  = name
-        self.nics[ name ]   = self.nic
+        id = self.get_nic_id( self.nic )
+        self.nic[ '_id' ] = id
+        self.nics[ id ]   = self.nic
         # Leave the 'self.nic' intact so we can display it later in
         # self.report()
         return
 
     def screen( self, candidates, name, value, same = True ):
+        if not candidates:
+            candidates = self.nics.keys()
         if same:
             candidates = [
-                key for key in candidates if
-                    self.nics[key].get( name, '_dunno' ) == value
+                id for id in candidates if
+                    self.nics[id].get( name, '_dunno' ) == value
             ]
         else:
             candidates = [
-                key for key in candidates if
-                    self.nics[key].get( name, '_dunno' ) != value
+                id for id in candidates if
+                    self.nics[id].get( name, '_dunno' ) != value
             ]
         return candidates
 
-    def set_used( self, key ):
-        self.nics[ key ][ '_used' ] = True
+    def set_used( self, id ):
+        self.nics[ id ][ '_used' ] = True
         return
 
-    def indent_print( self, s, indent = 0 ):
+    def indent_print( self, s, depth = 0 ):
         self.println(
             '{0}{1}'.format(
-                '    ' * indent,
+                '    ' * depth,
                 s,
             )
         )
         return
 
-    def _final_report( self ):
-        self.println()
-        title = 'S U M M A R Y'
-        self.println( title )
-        self.println( '=' * len( title ) )
-        # Pass 1: construct bridged interfaces
-        bridges = self.screen( self.nics.keys(), '_used', False )
-        bridges = self.screen( bridges, 'TYPE', 'Bridge' )
-        if len(bridges):
-            self.println()
-            title = 'Bridges'
-            self.println( title )
-            self.println( '-' * len( title ) )
-            for name in sorted( bridges ):
-                if 'DEVICE' in self.nics[ name ]:
-                    iface = self.nics[ name ]['DEVICE']
-                else:
-                    iface = name
-                self.indent_print( iface )
-                self.set_used( name )
-                paths = self.screen( self.nics.keys(), '_used', False )
-                paths = self.screen( paths, 'BRIDGE', iface )
-                if len( paths ):
-                    for path in sorted( paths ):
-                        self.indent_print( path, 1 )
-                        self.set_used( path )
-        bonds = self.screen( self.nics.keys(), '_used', False )
-        bonds = self.screen( bonds, 'TYPE', 'Bond' )
-        if len( bonds ):
-            self.println()
-            title = 'Bonded'
-            self.println( title )
-            self.println( '-' * len( title ) )
-            for name in sorted( bonds ):
-                if 'DEVICE' in self.nics[ name ]:
-                    iface = self.nics[ name ][ 'DEVICE' ]
-                else:
-                    iface = name
-                self.indent_print( iface )
-                self.set_used( name )
-        ethernets = self.screen( self.nics.keys(), '_used', False )
-        ethernets = self.screen( ethernets, 'TYPE', 'Ethernet' )
-        if len(ethernets):
-            self.println()
-            title = 'Ethernet'
-            self.println( title )
-            self.println( '-' * len(title) )
-            for name in ethernets:
-                if 'DEVICE' in self.nics[ name ]:
-                    iface = self.nics[ name ][ 'DEVICE' ]
-                else:
-                    iface = name
-                self.indent_print( iface )
-                self.set_used( name )
-        unclaimed = self.screen( self.nics.keys(), '_used', False )
-        unclaimed = self.screen( unclaimed, 'DEVICE', 'lo', False )
-        if len(unclaimed):
-            self.println()
-            title = 'Unprocessed NICs'
-            self.println( title )
-            self.println( '-' * len( title ) )
-            for name in sorted( unclaimed ):
-                self.indent_print( name )
-                self.set_used( name )
-        return
-
-    def _nic_report( self ):
+    def _print_a_nic( self, nic, depth = 0 ):
         # Output iface lines, sorted in order
         keys = [
             key for key in self.nic if key[0].isupper()
@@ -157,17 +99,88 @@ class   PrettyPrint( superclass.MetaPrettyPrinter ):
             value = self.nic[ key ]
             # delim = "'" if '"' in value else '"'
             delim = '"'
-            self.println(
+            self.indent_print(
                 fmt.format(
                     key,
                     '{0}{1}{0}'.format( delim, value )
-                )
+                ),
+                depth
             )
+        return
+
+    def _print_a_bridge( self, bridge, depth = 0 ):
+        self.indent_print( bridge, depth )
+        candidates = self.screen( None, '_used', False )
+        candidates = self.screen( candidates, 'BRIDGE', bridge )
+        ethernets  = self.screen( candidates, 'Type', 'Ethernet' )
+        for ethernet in sorted( ethernets ):
+            self.indent_print( ethernet, depth + 1 )
+            self.set_used( ethernet )
+            del candidates[ ethernet ]
+        if len(candidates):
+            print >>sys.stderr, 'unused bridge components={0}'.format(
+                candidates
+            )
+        return
+
+    def _print_a_bond( self, bond, depth = 0 ):
+        self.indent_print( bond, depth )
+        candidates = self.screen( None, '_used', False )
+        candidates = self.screen( candidates, 'SLAVE', 'yes' )
+        candidates = self.screen( candidates, 'MASTER', bond )
+        for slave in sorted( candidates ):
+            self.indent_print( slave, depth + 1 )
+            self.set_used( slave )
+        return
+
+    def _final_report( self ):
+        self.println()
+        title = 'S U M M A R Y'
+        self.println( title )
+        self.println( '=' * len( title ) )
+        # Step 0: The network (tm)
+        depth = 0
+        self.indent_print( 'network', depth )
+        # Step 1: construct bridged interfaces
+        bridges = self.screen( None, '_used', False )
+        bridges = self.screen( bridges, 'TYPE', 'Bridge' )
+        if len(bridges):
+            for bridge in sorted( bridges ):
+                self.set_used( bridge )
+                self._print_a_bridge( bridge, depth + 1 )
+        # Step 2: Bonded interfaces
+        bonds = self.screen( None, '_used', False )
+        bonds = self.screen( bonds, 'TYPE', 'Bond' )
+        if len( bonds ):
+            for bond in sorted( bonds ):
+                self.set_used( bond )
+                self._print_a_bond( bond, depth + 1 )
+        # Step 3: Plain Ethernets (Infiniband?)
+        ethernets = self.screen( None, '_used', False )
+        ethernets = self.screen( ethernets, 'TYPE', 'Ethernet' )
+        if len(ethernets):
+            for nic in ethernets:
+                self.set_used( nic )
+                if nic != 'lo':
+                    self.indent_print(
+                        nic,
+                        depth + 1
+                    )
+        # Step 4: Show any left-overs
+        unclaimed = self.screen( None, '_used', False )
+        unclaimed = self.screen( unclaimed, 'DEVICE', 'lo', False )
+        if len(unclaimed):
+            for name in sorted( unclaimed ):
+                self.set_used( name )
+                self.indent_print( name, depth + 1 )
         return
 
     def report( self, final = False ):
         if final:
             self._final_report()
         else:
-            self._nic_report()
+            id = self.nic.get( 'NAME', None )
+            if not id:
+                id = self.nic.get( 'DEVICE', 'DUNNO' )
+            self._print_a_nic( id )
         return
